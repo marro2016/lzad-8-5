28524de3e7f5d41d06b64af30ffd15fde81c1801
Marcin Różański <rozanskimarcin2016@gmail.com>
2018-03-10 17:34:16 +0100

    Add code js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('wartosc podstawy trojkata "a" wynosi');
+var h = prompt('wartosc podstawy trojkata "h" wynosi');
+var triangleArea = a*h/2;
+
+console.log("Pole trojkata o podstawie a: " + a + "i wysokosci h: " + h + 
+            " jest rowne: " + triangleArea);
